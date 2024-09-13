cd4870ae264fe1f997ad8b27d34a73d5ddf3ad77
Jean Olmedillo <78714438+Jmofuture@users.noreply.github.com>
2024-09-13 22:32:07 +0000

    Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function generadorDominios() {
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        console.log(`${p}${a}${n}.com`);
+      }
+    }
+  }
+}
+
+generadorDominios();
